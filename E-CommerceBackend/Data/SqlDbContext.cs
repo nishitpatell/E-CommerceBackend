5b30a1a3b4c3158ae420ae0b0b86825fb239b1df
NPatel <NPatel@M-PC11053>
2025-09-10 14:49:05 +0530

    Add project files.

---
+++ E-CommerceBackend/Data/SqlDbContext.cs
@@ -0,0 +1,15 @@
+﻿using E_CommerceBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceBackend.Data
+{
+    public class SqlDbContext : DbContext 
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ E-CommerceBackend/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceBackend.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter name..")]
+        public string Name { get; set; }
+    }
+}
