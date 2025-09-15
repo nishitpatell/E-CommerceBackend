9cb458e5dda264285d5897d91b89a0f3146880de
Nishit Patel <pnishit238@gmail.com>
2025-09-15 17:09:03 +0530

    Created Product Table and implemented ProductDtos.

---
+++ E-CommerceBackend/Dtos/ProductDtos/ProductDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_CommerceBackend.Dtos.ProductDtos
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public string? ProductDescription { get; set; }
+
+        public string? SpecialTag { get; set; }
+        
+        public int CategoryId { get; set; }
+
+        public string? ProductImageUrl { get; set; }
+    }
+}
