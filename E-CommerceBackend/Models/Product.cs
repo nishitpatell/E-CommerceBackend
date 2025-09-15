bcc4bcde7e50fdc021b11b1ce1214f4afb50df6a
Nishit Patel <pnishit238@gmail.com>
2025-09-15 15:52:26 +0530

    Implemented the Product model and IProductRepository

---
+++ E-CommerceBackend/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_CommerceBackend.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+
+        [Range(0.01, 100000)]
+        public decimal ProductPrice { get; set; }
+
+        public string? ProductDescription {  get; set; }
+
+        public string? SpecialTag { get; set; }
+        [ForeignKey("CategoryId")]
+        public int CategoryId { get; set; }
+
+        public string? ProductImageUrl { get; set; }    
+    }
+}
---
+++ E-CommerceBackend/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using E_CommerceBackend.Models;
+
+namespace E_CommerceBackend.Repository.IRepository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product> GetProductByIdAsync(int id);
+        Task<Product> CreateProductAsync(Product obj);
+        Task<Product> UpdateProductAsync(Product obj);
+        Task<bool> DeleteProductAsync(int id);
+    }
+}
