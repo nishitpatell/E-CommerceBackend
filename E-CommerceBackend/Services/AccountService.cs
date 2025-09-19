8d81963faf76af802edfd94f5cd41632f21a1016
Nishit Patel <pnishit238@gmail.com>
2025-09-19 17:10:03 +0530

    started implementing authentication. Created ApiUser Model and AuthDtos, Implemented services(AccountService) and registered it in Program.cs

---
+++ E-CommerceBackend/Services/AccountService.cs
@@ -0,0 +1,74 @@
+﻿using E_CommerceBackend.Dtos.AuthDtos;
+using E_CommerceBackend.Models;
+using E_CommerceBackend.Services.IServices;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace E_CommerceBackend.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly UserManager<ApiUser> _userManager;
+        private readonly SignInManager<ApiUser> _signInManager; 
+        private readonly IConfiguration _configuration;
+
+        public AccountService(
+            UserManager<ApiUser> userManager,
+            SignInManager<ApiUser> signInManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;            
+        }
+
+        public async Task<bool> RegisterAsync(RegisterDto registerDto)
+        {
+            var user = new ApiUser { UserName = registerDto.Email, Email = registerDto.Email }; 
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            return result.Succeeded;
+        }
+
+        public async Task<string> LoginAsync(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email); 
+            if (user == null)
+            {
+                return null;
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if(!result.Succeeded)
+            {
+                return null;
+            }
+
+            return GenerateJwtToken(user);
+        }
+
+        private string GenerateJwtToken(ApiUser user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
